c8feecbd33716cea859ed1f268d323ad470a0add
AbrQ <abrahamqt@gmail.com>
2022-01-27 20:15:02 -0600

    Add datatypes02 playground into repository

---
+++  DataTypes02.playground/Contents.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+let hello: String = "Hello World!"
+let pi: Double = 3.1416
+
+let number: Double = 3
+print(number)
+
+
+struct Guitar{
+    var strings: Int
+    var model: String
+    var color: String
+}
+
+Guitar.init(strings: 6, model: "Fender", color: "Orange")
+
+
